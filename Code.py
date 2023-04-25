ae46be91f58d6acd6515c6b011445d02995c54ac
Munisamy <MMunisamy@INTERNAL.COLT.NET>
2023-04-25 15:57:10 +0530

    New commit

---
+++ Code.py
@@ -0,0 +1,14 @@
+lst = [1,2,3,4,5,6,7,8,9,10]
+for i in lst:
+    if (i%2 == 0):
+        even_sum = even_sum+i
+    else:
+        odd_sum = odd_sum+i
+print(even_sum)
+print(odd_sum)
+
+
+num = 12
+for i in range(1,11):
+    print(num,x,i,=,num*i)
+    print(num x i = )
